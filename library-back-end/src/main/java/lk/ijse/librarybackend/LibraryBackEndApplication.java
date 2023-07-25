54651d05960abe3124329c2fcf325af1eaab13bf
Isuru <isurusajith0@gmail.com>
2023-07-25 14:52:27 +0530

    intialize back end with start spring io

---
+++ library-back-end/src/main/java/lk/ijse/librarybackend/LibraryBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.librarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBackEndApplication.class, args);
+	}
+
+}
